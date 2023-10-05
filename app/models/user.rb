e211217be0a465c9ce589e79fca77017ec4cc858
Javivy <javierralejandroortga@gmail.com>
2023-10-05 03:25:20 -0400

    make required changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :posts, class_name: 'post', foreign_key: 'authors_id'
-  has_many :comments, class_name: 'comment', foreign_key: 'authors_id'
-  has_many :likes, class_name: 'like', foreign_key: 'authors_id'
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'user_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'user_id'
 
   def recent_posts
     Post.where(authors_id: id).limit(3).order(created_at: :asc).pluck(:title, :text)
