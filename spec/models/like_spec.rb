858dc954b23d59eeb6f1fc6b58c610f92ba5ab9b
Javivy <javierralejandroortga@gmail.com>
2023-10-06 23:13:37 -0400

    Create tests for Like model and its methods and validations

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = User.create(
+      name: 'Example User',
+      posts_counter: 0
+    )
+    post = Post.create(
+      title: 'Example Post',
+      comments_counter: 0,
+      likes_counter: 0,
+      author_id: user.id
+    )
+    like = Like.new(
+      user_id: user.id,
+      post_id: post.id
+    )
+    expect(like).to be_valid
+  end
+
+  it 'increments the post\'s likes_counter after saving' do
+    user = User.create(
+      name: 'Example User',
+      posts_counter: 0
+    )
+    post = Post.create(
+      title: 'Example Post',
+      text: 'Hello',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: user
+    )
+    like = Like.create(
+      user: user,
+      post: post
+    )
+
+    expect(post.likes_counter).to eq(1)
+  end
 end
