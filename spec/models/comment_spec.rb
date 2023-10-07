657c2722040337bf6f7c7551c9d364cd551de1db
Javivy <javierralejandroortga@gmail.com>
2023-10-06 23:11:26 -0400

    Create tests for Comment model and its methods and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = User.create(
+      name: 'Example User',
+      posts_counter: 0
+    )
+    post = Post.create(
+      title: 'Example Post',
+      comments_counter: 0,
+      likes_counter: 0,
+      author_id: user.id
+    )
+    comment = Comment.new(
+      text: 'Example Comment',
+      user_id: user.id,
+      post_id: post.id
+    )
+    expect(comment).to be_valid
+  end
+
+  it 'is not valid without text' do
+    user = User.create(
+      name: 'Example User',
+      posts_counter: 0
+    )
+    post = Post.create(
+      title: 'Example Post',
+      comments_counter: 0,
+      likes_counter: 0,
+      author_id: user.id
+    )
+    comment = Comment.new(
+      text: nil,
+      user_id: user.id,
+      post_id: post.id
+    )
+    expect(comment).not_to be_valid
+  end
+
+  it 'increments the post\'s comments_counter after saving' do
+    user = User.create(
+      name: 'Example User',
+      posts_counter: 0
+    )
+    post = Post.create(
+      title: 'Example Post',
+      text: 'Hello',
+      comments_counter: 0,
+      likes_counter: 0,
+      author: user
+    )
+    comment = Comment.create(
+      text: 'Example Comment',
+      user: user,
+      post: post
+    )
+
+    expect(post.comments_counter).to eq(1)
+  end
 end
