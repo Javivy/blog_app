257e2e032b74ae1b7d4e2e6ad3919109e0f45fa4
Javivy <javierralejandroortga@gmail.com>
2023-10-12 12:03:29 -0400

    Create tests for posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns a 200 status code" do
+      get "/users/123/posts"
+
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the index template" do
+      get "/users/123/posts"
+
+      expect(response).to render_template('posts/index')
+    end
+
+    it "includes the correct placeholder text" do
+      get "/users/123/posts"
+      expect(response).to be_successful
+
+      doc = Nokogiri::HTML(response.body)
+
+      expect(doc.text).to include('Here\'s a list of posts for a given user with the id 123')
+    end
   end
 end
