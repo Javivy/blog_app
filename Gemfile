46dc05eff361ba271e3d438e60a62e5bc4c29e86
Javivy <javierralejandroortga@gmail.com>
2023-10-12 11:58:39 -0400

    Create tests for users controller

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
+gem 'rails-controller-testing'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns a 200 status code" do
+      get "/users/"
+
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the index template" do
+      get "/users/"
+
+      expect(response).to render_template('users/index')
+    end
+
+    it "includes the correct placeholder text" do
+      get "/users/"
+      expect(response).to be_successful
+
+      doc = Nokogiri::HTML(response.body)
+
+      expect(doc.text).to include('Users index page')
+    end
   end
 end
