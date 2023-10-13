f3587b0482a945e546cd72f2e728651f7b20fd71
Javivy <javierralejandroortga@gmail.com>
2023-10-13 01:00:33 -0400

    Add pagination for /users/:id/posts/ route, it appears when there's more than 5 posts

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
 gem 'rails-controller-testing'
+gem 'will_paginate'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id])
+    @posts = Post.where(author_id: params[:user_id]).paginate(page: params[:page], per_page: 5)
   end
 
   def show
