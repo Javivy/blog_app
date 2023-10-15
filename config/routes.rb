deaf6100a760899fdb06aa5f10ae2905fbe17acd
Javivy <javierralejandroortga@gmail.com>
2023-10-14 22:10:48 -0400

    Add like button and allow users to like a post

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "users#index"
 
   resources :users do
     resources :posts do
-      member do
-        post 'like'
-        delete 'unlike'
-      end
+      resources :likes, only: [:new, :create]
       resources :comments
     end
   end
