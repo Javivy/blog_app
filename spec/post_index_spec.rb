19f0722840376e370f81fcfc9b1242c190c895df
Javivy <javierralejandroortga@gmail.com>
2023-10-18 16:56:21 -0400

    Add tests for post index page 'shows the image of the user'

--- spec/post_index_spec.rb
+++ spec/post_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :feature do
   before do
-    @user = FactoryBot.create(:user, name: 'Masuma', photo_url: 'https://robohash.org/taryn.wehner.png?size=120x120&set=set1', posts_counter: 0)
+    @user = FactoryBot.create(:user, name: 'Masuma', photo: 'https://robohash.org/taryn.wehner.png?size=120x120&set=set1', posts_counter: 0)
     @post = FactoryBot.create(:post, title: 'Sample Post', text: 'Lorem ipsum', author: @user)
     @comment = FactoryBot.create(:comment, text: 'Sample comment', user: @user, post: @post)
     @second_post = FactoryBot.create(:post, title: 'Sample Post', text: 'Lorem ipsum', author: @user)
@@ -15,7 +15,7 @@ RSpec.describe 'Posts', type: :feature do
   describe 'index page' do
     it 'shows the image of the user' do
       visit user_posts_path(user_id: @user.id)
-      expect(page).to have_xpath(".//img[@src='#{@user.photo_url}']")
+      expect(page).to have_xpath(".//img[@src='#{@user.photo}']")
     end
 
     # it 'shows the user\s username' do
